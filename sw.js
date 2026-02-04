502d199407cef990e10a5490a6d7c2a9e9653017
dadsondankstry-crypto <dadsondankstry@gmail.com>
2026-02-04 17:33:52 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,22 +2,30 @@ const CACHE_NAME = 'cronos-ultra-v5-cache';
 const assets = [
   './',
   './index.html',
+  './icon-192.png',
+  './icon-512.jpg',
   'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js'
 ];
 
-// Instalação e Cache dos arquivos estáticos
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
   );
 });
 
-// Estratégia: Tenta Rede, se falhar ou estiver offline, usa Cache
 self.addEventListener('fetch', e => {
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(e.request).then(response => {
+        const fetchPromise = fetch(e.request).then(networkResponse => {
+          if (e.request.url.startsWith('http')) {
+             cache.put(e.request, networkResponse.clone());
+          }
+          return networkResponse;
+        }).catch(() => response);
+        return response || fetchPromise;
+      });
+    })
   );
 });
