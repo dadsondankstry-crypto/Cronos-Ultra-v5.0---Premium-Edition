10ada9bc74fd2a65414f4d2706b140f11a97466e
dadsondankstry-crypto <dadsondankstry@gmail.com>
2026-02-04 18:49:50 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,31 +1,19 @@
-const CACHE_NAME = 'cronos-ultra-v5-cache';
-const assets = [
+const CACHE_NAME = 'cronos-ultra-v15';
+const ASSETS = [
   './',
   './index.html',
-  './icon-192.png',
-  './icon-512.jpg',
   'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js'
 ];
 
-self.addEventListener('install', e => {
+self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.match(e.request).then(response => {
-        const fetchPromise = fetch(e.request).then(networkResponse => {
-          if (e.request.url.startsWith('http')) {
-             cache.put(e.request, networkResponse.clone());
-          }
-          return networkResponse;
-        }).catch(() => response);
-        return response || fetchPromise;
-      });
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
